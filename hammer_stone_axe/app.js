b76a670f737f328a5105dffc270db40507cf0be2
Dominique <genjineedsmorehealing@gmail.com>
2019-07-30 16:42:09 -0500

    layout complete

--- hammer_stone_axe/app.js
+++ hammer_stone_axe/app.js
@@ -6,6 +6,8 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var battlegroundRouter = require('./routes/battleground');
+
 
 var app = express();
 
@@ -19,8 +21,11 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.engine('ejs', require('ejs-locals'));
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/battleground', battlegroundRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ hammer_stone_axe/routes/battleground.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+
+
+router.get('/', function (req, res, next) {
+	res.render('battleground', {title: 'Battleground'});
+});
+
+router.get('/', function(req, res, next){
+	res.redirect('/');
+})
+
+module.exports = router;
