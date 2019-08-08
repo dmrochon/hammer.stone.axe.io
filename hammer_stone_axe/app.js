fb8251fda5919ba7d5c33f1e5af3b023f4c6f290
Dominique <genjineedsmorehealing@gmail.com>
2019-08-08 13:48:29 -0500

    began Hall

--- hammer_stone_axe/app.js
+++ hammer_stone_axe/app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var battlegroundRouter = require('./routes/battleground');
 var p2_battlegroundRouter = require('./routes/p2_battleground');
+var championsRouter = require('./routes/champions');
 
 
 var app = express();
@@ -29,6 +30,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/battleground', battlegroundRouter);
 app.use('/p2_battleground', p2_battlegroundRouter);
+app.use('/champions', championsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- hammer_stone_axe/routes/champions.js
+++ hammer_stone_axe/routes/champions.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function (req, res, next) {
-	res.render('/champions', {title: 'Champions\' Hall'});
+	res.render('champions', {title: 'Champion\'s Hall'});
 });
 
 router.get('/', function(req, res, next){
